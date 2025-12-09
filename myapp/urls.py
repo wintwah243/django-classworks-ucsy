2eee67745ad8fe46532c7a1f3dab374366dabf6e
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-12-09 19:44:14 +0630

    exercise: regularexpression,template,route

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    re_path(r'^about/index/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('index/', views.index, name='index'),
+    path("blog/", views.page),
+    path("blog/page<int:num>/", views.page),
+    path("articles/2003/", views.special_case_2003),
+ 	path("articles/<int:year>/", views.year_archive),
+	path("articles/<int:year>/<int:month>/", views.month_archive),
+ 	path("articles/<int:year>/<int:month>/<slug:slug>/", views.article_detail),
+    path("articles/2003/", views.special_case_2003),
+	re_path(r"^articles/(?P<year>[0-9]{4})/$", views.year_archive),
+ 	re_path(r"^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$", views.month_archive),
+	re_path( r"^articles/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<slug>[\w-]+)/$", views.article_detail, ),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
+# Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls page.")
-# Create your views here.
+    return HttpResponse("Hello, world. You're at the home page.")
+
+def about(request):
+    return HttpResponse("Hello, You are at the about page.")
+
+def index(request):
+    now = datetime.datetime.now()
+    html = "<html><body><h1>Now time is %s.</h1></body></html>" % now
+    return HttpResponse(html)
+
+def page(request, num=1):
+    html = "<html><body><h3>Number of pages: %s.</h3></body></html> " % num
+    return HttpResponse(html)
+
+def special_case_2003(request):
+    return HttpResponse("this is a special case 2003")
+
+def year_archive(request, year):
+    return HttpResponse(f"Articles from {year}")
+
+def month_archive(request, year, month):
+    return HttpResponse(f"Articles from {month}/{year}")
+
+def article_detail(request, year, month, slug):
+    return HttpResponse(f"Detail page: {slug} ({month}/{year})")
