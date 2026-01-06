6a709da5aa3d918d128e0cd46c13e300f9153f4a
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-01-06 08:09:10 +0630

    Lecture: model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Store, StudentInfo, StudentDetail, Product
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Store)
+admin.site.register(StudentInfo)
+admin.site.register(StudentDetail)
+admin.site.register(Product)
--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Student(models.Model):
       name = models.CharField(max_length=30)
@@ -8,3 +9,45 @@ class Student(models.Model):
 
       def __str__(self):
         return self.name
+
+
+def default_city():
+    return "San Diego"
+
+class Store(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30, unique=True)
+    city = models.CharField(max_length=30, default=default_city)
+    state = models.CharField(max_length=2, default='CA')
+
+    def __str__(self):
+        return self.name
+
+class StudentInfo(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class StudentDetail(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Enter your full name as per official records",
+    )
+    age = models.IntegerField(
+        help_text="Age mush be 18 or above",
+    )
+
+def StartsWithAValidator(value):
+    if not value.startswith('A'):
+        raise ValidationError('Must start with "A"')
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        validators=[StartsWithAValidator],
+    )
+
+def clean(self):
+    if self.city == 'San Diego' and self.state != 'CA':
+        raise ValidationError('Wait San Diego is CA!, are you sure there is another San Diego in %s ?' % self.state)
