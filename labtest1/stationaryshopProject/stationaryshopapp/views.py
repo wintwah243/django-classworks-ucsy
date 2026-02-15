ee766b544cfb1eefb6b629b6a1c94a224e86dc74
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-02-15 11:28:58 +0630

    htamin

--- labtest1/stationaryshopProject/stationaryshopapp/views.py
+++ labtest1/stationaryshopProject/stationaryshopapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Product
 from .forms import StationaryForm
+# htamin thwr sar ml
 
 #111111
 def new(request):
