4ed86a0f2702c5886d201244e9a99ae0de0ffc9b
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2026-02-15 11:21:58 +0630

    :)

--- labtest1/stationaryshopProject/stationaryshopapp/views.py
+++ labtest1/stationaryshopProject/stationaryshopapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from .models import Product
 from .forms import StationaryForm
 
+# hta min thwr sar ml
+
 def new(request):
     if request.method == 'POST':
         stationary_form = StationaryForm(request.POST)
@@ -15,8 +17,6 @@ def new(request):
 
     return render(request, 'stationary/add.html', {'form': stationary_form})
 
-
-# this is stationary
 def stationary(request):
     data = Product.objects.all()
     return render(request, 'stationary/stationary.html', {'stationaries': data})
